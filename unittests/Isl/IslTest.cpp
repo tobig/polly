4e4361805eae7056e6feadce1af44952210b070d
Tobias Grosser <tobias@grosser.es>
2017-02-25 12:52:55 +0100

    [unittest] Translate isl tests to C++ bindings
    For this translation we introduce two functions, valFromAPInt and APIntFromVal,
    to convert between isl::Val and APInt. For now these are just proxies, but in
    the future they will replace the current isl_val* based conversion functions.
    
    The isl unit test cases benefit most from the new isl::Bool (from Michael
    Kruse), which can be explicitly casted to bool and which -- as part of this cast
    -- emits a check that no error condition has been triggered so far. This allows
    us to simplify
    
      EXPECT_EQ(isl_bool_true, isl_val_is_zero(IslZero));
    
    to
    
      EXPECT_TRUE(IslZero.isZero());
    
    This simplification also becomes very clear in operator==, which changes from
    
      auto IsEqual = isl_set_is_equal(LHS.keep(), RHS.keep());
      EXPECT_NE(isl_bool_error, IsEqual);
      return IsEqual;
    
    to just
    
      return bool(LHS.isEqual(RHS));
    
    Some background for non-isl users. The isl C interface has an isl_bool type,
    which can be either true, false, or error. Hence, whenever a function returns
    a value of type isl_bool, an explicit error check should be considered. By
    using isl::Bool, we can just cast the isl::Bool to 'bool' or simply use the
    isl::Bool in a context where it will automatically be casted to bool (e.g., in
    an if-condition). When doing so, the C++ bindings automatically add code that
    verifies that the return value is not an error code. If it is, the program will
    warn about this and abort. For cases where errors are expected, isl::Bool
    provides checks such as Bool::isTrueOrError() or Bool::isTrueNoError() to
    explicitly control program behavior in case of error conditions.
    
    Thanks to the new automatic memory management, we also can avoid many calls to
    isl_*_free. For code that had previously been managed by IslPtr<>, many calls
    to give/take/copy are eliminated.

--- unittests/Isl/IslTest.cpp
+++ unittests/Isl/IslTest.cpp
@@ -34,43 +34,45 @@ static isl::Space parseSpace(isl_ctx *Ctx, const char *Str) {
 
 #define SPACE(Str) parseSpace(Ctx.get(), Str)
 
-#define SET(Str) give(isl_set_read_from_str(Ctx.get(), Str))
-#define MAP(Str) give(isl_map_read_from_str(Ctx.get(), Str))
+#define SET(Str) isl::Set::readFromStr(Ctx.get(), Str)
+#define MAP(Str) isl::Map::readFromStr(Ctx.get(), Str)
 
-#define USET(Str) give(isl_union_set_read_from_str(Ctx.get(), Str))
-#define UMAP(Str) give(isl_union_map_read_from_str(Ctx.get(), Str))
+#define USET(Str) isl::UnionSet::readFromStr(Ctx.get(), Str)
+#define UMAP(Str) isl::UnionMap::readFromStr(Ctx.get(), Str)
 
 namespace isl {
 inline namespace noexceptions {
 
 static bool operator==(const isl::Space &LHS, const isl::Space &RHS) {
-  auto IsEqual = isl_space_is_equal(LHS.keep(), RHS.keep());
-  EXPECT_NE(isl_bool_error, IsEqual);
-  return IsEqual;
+  return bool(LHS.isEqual(RHS));
+}
+
+static bool operator==(const isl::BasicSet &LHS, const isl::BasicSet &RHS) {
+  return bool(LHS.isEqual(RHS));
 }
 
 static bool operator==(const isl::Set &LHS, const isl::Set &RHS) {
-  auto IsEqual = isl_set_is_equal(LHS.keep(), RHS.keep());
-  EXPECT_NE(isl_bool_error, IsEqual);
-  return IsEqual;
+  return bool(LHS.isEqual(RHS));
+}
+
+static bool operator==(const isl::BasicMap &LHS, const isl::BasicMap &RHS) {
+  return bool(LHS.isEqual(RHS));
 }
 
 static bool operator==(const isl::Map &LHS, const isl::Map &RHS) {
-  auto IsEqual = isl_map_is_equal(LHS.keep(), RHS.keep());
-  EXPECT_NE(isl_bool_error, IsEqual);
-  return IsEqual;
+  return bool(LHS.isEqual(RHS));
 }
 
 static bool operator==(const isl::UnionSet &LHS, const isl::UnionSet &RHS) {
-  auto IsEqual = isl_union_set_is_equal(LHS.keep(), RHS.keep());
-  EXPECT_NE(isl_bool_error, IsEqual);
-  return IsEqual;
+  return bool(LHS.isEqual(RHS));
 }
 
 static bool operator==(const isl::UnionMap &LHS, const isl::UnionMap &RHS) {
-  auto IsEqual = isl_union_map_is_equal(LHS.keep(), RHS.keep());
-  EXPECT_NE(isl_bool_error, IsEqual);
-  return IsEqual;
+  return bool(LHS.isEqual(RHS));
+}
+
+static bool operator==(const isl::Val &LHS, const isl::Val &RHS) {
+  return bool(LHS.eq(RHS));
 }
 }
 }
@@ -82,87 +84,73 @@ TEST(Isl, APIntToIslVal) {
 
   {
     APInt APZero(1, 0, true);
-    auto *IslZero = isl_valFromAPInt(IslCtx, APZero, true);
-    EXPECT_EQ(isl_bool_true, isl_val_is_zero(IslZero));
-    isl_val_free(IslZero);
+    auto IslZero = valFromAPInt(IslCtx, APZero, true);
+    EXPECT_TRUE(IslZero.isZero());
   }
 
   {
     APInt APNOne(1, -1, true);
-    auto *IslNOne = isl_valFromAPInt(IslCtx, APNOne, true);
-    EXPECT_EQ(isl_bool_true, isl_val_is_negone(IslNOne));
-    isl_val_free(IslNOne);
+    auto IslNOne = valFromAPInt(IslCtx, APNOne, true);
+    EXPECT_TRUE(IslNOne.isNegone());
   }
 
   {
     APInt APZero(1, 0, false);
-    auto *IslZero = isl_valFromAPInt(IslCtx, APZero, false);
-    EXPECT_EQ(isl_bool_true, isl_val_is_zero(IslZero));
-    isl_val_free(IslZero);
+    auto IslZero = valFromAPInt(IslCtx, APZero, false);
+    EXPECT_TRUE(IslZero.isZero());
   }
 
   {
     APInt APOne(1, 1, false);
-    auto *IslOne = isl_valFromAPInt(IslCtx, APOne, false);
-    EXPECT_EQ(isl_bool_true, isl_val_is_one(IslOne));
-    isl_val_free(IslOne);
+    auto IslOne = valFromAPInt(IslCtx, APOne, false);
+    EXPECT_TRUE(IslOne.isOne());
   }
 
   {
     APInt APNTwo(2, -2, true);
-    auto *IslNTwo = isl_valFromAPInt(IslCtx, APNTwo, true);
-    auto *IslNTwoCmp = isl_val_int_from_si(IslCtx, -2);
-    EXPECT_EQ(isl_bool_true, isl_val_eq(IslNTwo, IslNTwoCmp));
-    isl_val_free(IslNTwo);
-    isl_val_free(IslNTwoCmp);
+    auto IslNTwo = valFromAPInt(IslCtx, APNTwo, true);
+    auto IslNTwoCmp = isl::Val::intFromSi(IslCtx, -2);
+    EXPECT_EQ(IslNTwo, IslNTwoCmp);
   }
 
   {
     APInt APNOne(32, -1, true);
-    auto *IslNOne = isl_valFromAPInt(IslCtx, APNOne, true);
-    EXPECT_EQ(isl_bool_true, isl_val_is_negone(IslNOne));
-    isl_val_free(IslNOne);
+    auto IslNOne = valFromAPInt(IslCtx, APNOne, true);
+    EXPECT_TRUE(IslNOne.isNegone());
   }
 
   {
     APInt APZero(32, 0, false);
-    auto *IslZero = isl_valFromAPInt(IslCtx, APZero, false);
-    EXPECT_EQ(isl_bool_true, isl_val_is_zero(IslZero));
-    isl_val_free(IslZero);
+    auto IslZero = valFromAPInt(IslCtx, APZero, false);
+    EXPECT_TRUE(IslZero.isZero());
   }
 
   {
     APInt APOne(32, 1, false);
-    auto *IslOne = isl_valFromAPInt(IslCtx, APOne, false);
-    EXPECT_EQ(isl_bool_true, isl_val_is_one(IslOne));
-    isl_val_free(IslOne);
+    auto IslOne = valFromAPInt(IslCtx, APOne, false);
+    EXPECT_TRUE(IslOne.isOne());
   }
 
   {
     APInt APTwo(32, 2, false);
-    auto *IslTwo = isl_valFromAPInt(IslCtx, APTwo, false);
-    EXPECT_EQ(0, isl_val_cmp_si(IslTwo, 2));
-    isl_val_free(IslTwo);
+    auto IslTwo = valFromAPInt(IslCtx, APTwo, false);
+    EXPECT_EQ(0, IslTwo.cmpSi(2));
   }
 
   {
     APInt APNOne(32, (1ull << 32) - 1, false);
-    auto *IslNOne = isl_valFromAPInt(IslCtx, APNOne, false);
-    auto *IslRef = isl_val_int_from_ui(IslCtx, (1ull << 32) - 1);
-    EXPECT_EQ(isl_bool_true, isl_val_eq(IslNOne, IslRef));
-    isl_val_free(IslNOne);
-    isl_val_free(IslRef);
+    auto IslNOne = valFromAPInt(IslCtx, APNOne, false);
+    auto IslRef = isl::Val::intFromUi(IslCtx, (1ull << 32) - 1);
+    EXPECT_EQ(IslNOne, IslRef);
   }
 
   {
     APInt APLarge(130, 2, false);
     APLarge = APLarge.shl(70);
-    auto *IslLarge = isl_valFromAPInt(IslCtx, APLarge, false);
-    auto *IslRef = isl_val_int_from_ui(IslCtx, 71);
-    IslRef = isl_val_2exp(IslRef);
-    EXPECT_EQ(isl_bool_true, isl_val_eq(IslLarge, IslRef));
-    isl_val_free(IslLarge);
-    isl_val_free(IslRef);
+    auto IslLarge = valFromAPInt(IslCtx, APLarge, false);
+    auto IslRef = isl::Val::intFromUi(IslCtx, 71);
+    IslRef = IslRef.two_exp();
+    EXPECT_EQ(IslLarge, IslRef);
   }
 
   isl_ctx_free(IslCtx);
@@ -172,7 +160,7 @@ TEST(Isl, IslValToAPInt) {
   isl_ctx *IslCtx = isl_ctx_alloc();
 
   {
-    auto *IslNOne = isl_val_int_from_si(IslCtx, -1);
+    auto IslNOne = isl::Val::intFromSi(IslCtx, -1);
     auto APNOne = APIntFromVal(IslNOne);
     // Compare with the two's complement of -1 in a 1-bit integer.
     EXPECT_EQ(1, APNOne);
@@ -180,7 +168,7 @@ TEST(Isl, IslValToAPInt) {
   }
 
   {
-    auto *IslNTwo = isl_val_int_from_si(IslCtx, -2);
+    auto IslNTwo = isl::Val::intFromSi(IslCtx, -2);
     auto APNTwo = APIntFromVal(IslNTwo);
     // Compare with the two's complement of -2 in a 2-bit integer.
     EXPECT_EQ(2, APNTwo);
@@ -188,7 +176,7 @@ TEST(Isl, IslValToAPInt) {
   }
 
   {
-    auto *IslNThree = isl_val_int_from_si(IslCtx, -3);
+    auto IslNThree = isl::Val::intFromSi(IslCtx, -3);
     auto APNThree = APIntFromVal(IslNThree);
     // Compare with the two's complement of -3 in a 3-bit integer.
     EXPECT_EQ(5, APNThree);
@@ -196,7 +184,7 @@ TEST(Isl, IslValToAPInt) {
   }
 
   {
-    auto *IslNFour = isl_val_int_from_si(IslCtx, -4);
+    auto IslNFour = isl::Val::intFromSi(IslCtx, -4);
     auto APNFour = APIntFromVal(IslNFour);
     // Compare with the two's complement of -4 in a 3-bit integer.
     EXPECT_EQ(4, APNFour);
@@ -204,59 +192,59 @@ TEST(Isl, IslValToAPInt) {
   }
 
   {
-    auto *IslZero = isl_val_int_from_ui(IslCtx, 0);
+    auto IslZero = isl::Val::intFromUi(IslCtx, 0);
     auto APZero = APIntFromVal(IslZero);
     EXPECT_EQ(0, APZero);
     EXPECT_EQ(1u, APZero.getBitWidth());
   }
 
   {
-    auto *IslOne = isl_val_int_from_ui(IslCtx, 1);
+    auto IslOne = isl::Val::intFromUi(IslCtx, 1);
     auto APOne = APIntFromVal(IslOne);
     EXPECT_EQ(1, APOne);
     EXPECT_EQ(2u, APOne.getBitWidth());
   }
 
   {
-    auto *IslTwo = isl_val_int_from_ui(IslCtx, 2);
+    auto IslTwo = isl::Val::intFromUi(IslCtx, 2);
     auto APTwo = APIntFromVal(IslTwo);
     EXPECT_EQ(2, APTwo);
     EXPECT_EQ(3u, APTwo.getBitWidth());
   }
 
   {
-    auto *IslThree = isl_val_int_from_ui(IslCtx, 3);
+    auto IslThree = isl::Val::intFromUi(IslCtx, 3);
     auto APThree = APIntFromVal(IslThree);
     EXPECT_EQ(3, APThree);
     EXPECT_EQ(3u, APThree.getBitWidth());
   }
 
   {
-    auto *IslFour = isl_val_int_from_ui(IslCtx, 4);
+    auto IslFour = isl::Val::intFromUi(IslCtx, 4);
     auto APFour = APIntFromVal(IslFour);
     EXPECT_EQ(4, APFour);
     EXPECT_EQ(4u, APFour.getBitWidth());
   }
 
   {
-    auto *IslNOne = isl_val_int_from_ui(IslCtx, (1ull << 32) - 1);
+    auto IslNOne = isl::Val::intFromUi(IslCtx, (1ull << 32) - 1);
     auto APNOne = APIntFromVal(IslNOne);
     EXPECT_EQ((1ull << 32) - 1, APNOne);
     EXPECT_EQ(33u, APNOne.getBitWidth());
   }
 
   {
-    auto *IslLargeNum = isl_val_int_from_ui(IslCtx, 60);
-    IslLargeNum = isl_val_2exp(IslLargeNum);
-    IslLargeNum = isl_val_sub_ui(IslLargeNum, 1);
+    auto IslLargeNum = isl::Val::intFromUi(IslCtx, 60);
+    IslLargeNum = IslLargeNum.two_exp();
+    IslLargeNum = IslLargeNum.subUi(1);
     auto APLargeNum = APIntFromVal(IslLargeNum);
     EXPECT_EQ((1ull << 60) - 1, APLargeNum);
     EXPECT_EQ(61u, APLargeNum.getBitWidth());
   }
 
   {
-    auto *IslExp = isl_val_int_from_ui(IslCtx, 500);
-    auto *IslLargePow2 = isl_val_2exp(IslExp);
+    auto IslExp = isl::Val::intFromUi(IslCtx, 500);
+    auto IslLargePow2 = IslExp.two_exp();
     auto APLargePow2 = APIntFromVal(IslLargePow2);
     EXPECT_TRUE(APLargePow2.isPowerOf2());
     EXPECT_EQ(502u, APLargePow2.getBitWidth());
@@ -264,8 +252,8 @@ TEST(Isl, IslValToAPInt) {
   }
 
   {
-    auto *IslExp = isl_val_int_from_ui(IslCtx, 500);
-    auto *IslLargeNPow2 = isl_val_neg(isl_val_2exp(IslExp));
+    auto IslExp = isl::Val::intFromUi(IslCtx, 500);
+    auto IslLargeNPow2 = IslExp.two_exp().neg();
     auto APLargeNPow2 = APIntFromVal(IslLargeNPow2);
     EXPECT_EQ(501u, APLargeNPow2.getBitWidth());
     EXPECT_EQ(501u, APLargeNPow2.getMinSignedBits());
@@ -273,8 +261,8 @@ TEST(Isl, IslValToAPInt) {
   }
 
   {
-    auto *IslExp = isl_val_int_from_ui(IslCtx, 512);
-    auto *IslLargePow2 = isl_val_2exp(IslExp);
+    auto IslExp = isl::Val::intFromUi(IslCtx, 512);
+    auto IslLargePow2 = IslExp.two_exp();
     auto APLargePow2 = APIntFromVal(IslLargePow2);
     EXPECT_TRUE(APLargePow2.isPowerOf2());
     EXPECT_EQ(514u, APLargePow2.getBitWidth());
@@ -282,8 +270,8 @@ TEST(Isl, IslValToAPInt) {
   }
 
   {
-    auto *IslExp = isl_val_int_from_ui(IslCtx, 512);
-    auto *IslLargeNPow2 = isl_val_neg(isl_val_2exp(IslExp));
+    auto IslExp = isl::Val::intFromUi(IslCtx, 512);
+    auto IslLargeNPow2 = IslExp.two_exp().neg();
     auto APLargeNPow2 = APIntFromVal(IslLargeNPow2);
     EXPECT_EQ(513u, APLargeNPow2.getBitWidth());
     EXPECT_EQ(513u, APLargeNPow2.getMinSignedBits());
@@ -297,24 +285,22 @@ TEST(Isl, Foreach) {
   std::unique_ptr<isl_ctx, decltype(&isl_ctx_free)> Ctx(isl_ctx_alloc(),
                                                         &isl_ctx_free);
 
-  auto MapSpace = give(isl_space_alloc(Ctx.get(), 0, 1, 1));
-  auto TestBMap = give(isl_basic_map_universe(MapSpace.copy()));
-  TestBMap = give(isl_basic_map_fix_si(TestBMap.take(), isl_dim_in, 0, 0));
-  TestBMap = give(isl_basic_map_fix_si(TestBMap.take(), isl_dim_out, 0, 0));
-  auto TestMap = give(isl_map_from_basic_map(TestBMap.copy()));
-  auto TestUMap = give(isl_union_map_from_map(TestMap.copy()));
+  auto MapSpace = isl::Space::alloc(Ctx.get(), 0, 1, 1);
+  auto TestBMap = isl::BasicMap::universe(MapSpace);
+  TestBMap = TestBMap.fixSi(isl::Dim::Out, 0, 0);
+  TestBMap = TestBMap.fixSi(isl::Dim::Out, 0, 0);
+  isl::Map TestMap = TestBMap;
+  isl::UnionMap TestUMap = TestMap;
 
-  auto SetSpace = give(isl_space_set_alloc(Ctx.get(), 0, 1));
-  auto TestBSet =
-      give(isl_basic_set_from_point(isl_point_zero(SetSpace.copy())));
-  auto TestSet = give(isl_set_from_basic_set(TestBSet.copy()));
-  auto TestUSet = give(isl_union_set_from_set(TestSet.copy()));
+  auto SetSpace = isl::Space::setAlloc(Ctx.get(), 0, 1);
+  isl::BasicSet TestBSet = isl::Point::zero(SetSpace);
+  isl::Set TestSet = TestBSet;
+  isl::UnionSet TestUSet = TestSet;
 
   {
     auto NumBMaps = 0;
     foreachElt(TestMap, [&](isl::BasicMap BMap) {
-      EXPECT_EQ(isl_bool_true,
-                isl_basic_map_is_equal(BMap.keep(), TestBMap.keep()));
+      EXPECT_EQ(BMap, TestBMap);
       NumBMaps++;
     });
     EXPECT_EQ(1, NumBMaps);
@@ -323,8 +309,7 @@ TEST(Isl, Foreach) {
   {
     auto NumBSets = 0;
     foreachElt(TestSet, [&](isl::BasicSet BSet) {
-      EXPECT_EQ(isl_bool_true,
-                isl_basic_set_is_equal(BSet.keep(), TestBSet.keep()));
+      EXPECT_EQ(BSet, TestBSet);
       NumBSets++;
     });
     EXPECT_EQ(1, NumBSets);
@@ -333,7 +318,7 @@ TEST(Isl, Foreach) {
   {
     auto NumMaps = 0;
     foreachElt(TestUMap, [&](isl::Map Map) {
-      EXPECT_EQ(isl_bool_true, isl_map_is_equal(Map.keep(), TestMap.keep()));
+      EXPECT_EQ(Map, TestMap);
       NumMaps++;
     });
     EXPECT_EQ(1, NumMaps);
@@ -342,18 +327,18 @@ TEST(Isl, Foreach) {
   {
     auto NumSets = 0;
     foreachElt(TestUSet, [&](isl::Set Set) {
-      EXPECT_EQ(isl_bool_true, isl_set_is_equal(Set.keep(), TestSet.keep()));
+      EXPECT_EQ(Set, TestSet);
       NumSets++;
     });
     EXPECT_EQ(1, NumSets);
   }
 
   {
-    auto UPwAff = give(isl_union_pw_aff_val_on_domain(TestUSet.copy(),
-                                                      isl_val_zero(Ctx.get())));
+    auto UPwAff =
+        isl::UnionPwAff::valOnDomain(TestUSet, isl::Val::zero(Ctx.get()));
     auto NumPwAffs = 0;
     foreachElt(UPwAff, [&](isl::PwAff PwAff) {
-      EXPECT_EQ(isl_bool_true, isl_pw_aff_is_cst(PwAff.keep()));
+      EXPECT_TRUE(PwAff.isCst());
       NumPwAffs++;
     });
     EXPECT_EQ(1, NumPwAffs);
@@ -363,8 +348,7 @@ TEST(Isl, Foreach) {
     auto NumBMaps = 0;
     EXPECT_EQ(isl_stat_error,
               foreachEltWithBreak(TestMap, [&](isl::BasicMap BMap) -> isl_stat {
-                EXPECT_EQ(isl_bool_true,
-                          isl_basic_map_is_equal(BMap.keep(), TestBMap.keep()));
+                EXPECT_EQ(BMap, TestBMap);
                 NumBMaps++;
                 return isl_stat_error;
               }));
@@ -375,8 +359,7 @@ TEST(Isl, Foreach) {
     auto NumMaps = 0;
     EXPECT_EQ(isl_stat_error,
               foreachEltWithBreak(TestUMap, [&](isl::Map Map) -> isl_stat {
-                EXPECT_EQ(isl_bool_true,
-                          isl_map_is_equal(Map.keep(), TestMap.keep()));
+                EXPECT_EQ(Map, TestMap);
                 NumMaps++;
                 return isl_stat_error;
               }));
@@ -385,16 +368,15 @@ TEST(Isl, Foreach) {
 
   {
     auto TestPwAff =
-        give(isl_pw_aff_val_on_domain(TestSet.copy(), isl_val_zero(Ctx.get())));
+        isl::PwAff::valOnDomain(TestSet, isl::Val::zero(Ctx.get()));
     auto NumPieces = 0;
-    foreachPieceWithBreak(
-        TestPwAff, [&](isl::Set Domain, isl::Aff Aff) -> isl_stat {
-          EXPECT_EQ(isl_bool_true,
-                    isl_set_is_equal(Domain.keep(), TestSet.keep()));
-          EXPECT_EQ(isl_bool_true, isl_aff_is_cst(Aff.keep()));
-          NumPieces++;
-          return isl_stat_error;
-        });
+    foreachPieceWithBreak(TestPwAff,
+                          [&](isl::Set Domain, isl::Aff Aff) -> isl_stat {
+                            EXPECT_EQ(Domain, TestSet);
+                            EXPECT_TRUE(Aff.isCst());
+                            NumPieces++;
+                            return isl_stat_error;
+                          });
     EXPECT_EQ(1, NumPieces);
   }
 }
